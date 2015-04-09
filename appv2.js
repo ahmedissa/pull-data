31fdcc12e6f02e4f116f7529a4d0979adbcf2ad1
ahmedissa <neuissa@gmail.com>
2015-04-09 23:29:27 +0200

    init project

---
+++ appv2.js
@@ -0,0 +1,143 @@
+
+require('dotenv').load();
+
+var mongo = require('mongoskin');
+var CronJob = require('cron').CronJob;
+var moment = require('moment');
+var request = require('request');
+
+var Server = mongo.Server;
+
+var db = mongo.db("mongodb://localhost:27017/riot_data", {native_parser:true});
+
+db.bind('matchId');
+
+db.bind('matchs');
+
+
+var jobPullMatchIds = new CronJob({
+  cronTime: '*/5 * * * *',
+  onTick: function() {
+		var minValue, date, url; 
+		date = moment().subtract(30, 'minutes');
+		minValue = date.get('minute');
+		minValue = Math.floor(minValue / 5);
+		minValue = minValue * 5;
+
+		date.set('minute', minValue);
+		date.set('second', 0);
+
+		url = '/api/lol/euw/v4.1/game/ids?' + 'beginDate='+  date.unix() + '&api_key='+ process.env.API_KEY;
+
+	  request(
+	    { method: 'GET'
+	    , url: process.env.API_ROOT_URL + url,
+	    json: true
+	    }
+	  , function (error, response, body) {
+	      // body is the decompressed response body 
+
+	      if (error) {
+					console.log(date.format("dddd, MMMM Do YYYY, h:mm:ss a"), date.unix(),  url, 
+				  									 "error: " + error.message);
+	      	return; 
+	      }
+	      console.log(response.statusCode);
+
+				if (response.statusCode === 200) {
+						console.log('#add match ids')
+
+						var data  = [];
+						for (var i = body.length - 1; i >= 0; i--) {
+							data.push( {matchId: body[i]});
+						};
+
+						db.matchId.insert(data,function (e, results) {
+							if (e) {
+
+								console.log('#add match ids error', e.message);
+
+							};
+								console.log('#add match ids ', results);
+
+						})
+			  };
+
+
+				console.log(date.format("dddd, MMMM Do YYYY, h:mm:ss a"), date.unix(),  url, 
+			  									 "Got response: " + response.statusCode);
+	    }
+	  )
+
+  },
+  start: false,
+  timeZone: 'Europe/Berlin'
+});
+
+jobPullMatchIds.start();
+
+
+jobPullMatchData = new CronJob({
+  cronTime: '*/1 * * * *',
+  onTick: function() {
+		console.log('get matchs ', moment().format("dddd, MMMM Do YYYY, h:mm:ss a"))
+
+		db.matchId.find().limit( 30 ).toArray(function(err, items) {
+			var url;
+
+			//  should be run in parallel
+
+			url = '/api/lol/euw/v2.2/match/';
+			for (var i = items.length - 1; i >= 0; i--) {
+				(function (currentMatchId) {
+				  request(
+				    { method: 'GET',
+				    	json: true,
+				      url: process.env.API_ROOT_URL + url + currentMatchId +  '?api_key='+ process.env.API_KEY + '&includeTimeline=true'
+				    }
+				  , function (error, response, body) {
+				  		console.log( process.env.API_ROOT_URL + url + '?api_key='+ process.env.API_KEY + '&includeTimeline=true');
+
+				      if (error) {
+								console.log(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"), moment().unix(),  url, 
+							  									 "error: " + error.message);
+				      	return; 
+				      }
+
+
+							if (response.statusCode === 200) {
+									db.matchs.update({matchId: body.matchId},body, {upsert: true},function(e, results){
+
+								    if (e){
+											console.log(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"), moment().unix(),  url, 
+										  									 "error: " + e.message);
+								    	return;
+								    }
+
+								    db.matchId.remove({matchId: currentMatchId}, { w: 0 });
+
+								  })
+						  } else if (! (response.statusCode === 429 )) {
+						  	// match is not reachable 
+						  	console.log(response.statusCode);
+								db.matchId.remove({matchId: currentMatchId}, { w: 0 });
+						  }
+
+							console.log(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"), moment().unix(),  url, 
+						  									 "Got response: " + response.statusCode);
+				    }
+				  )
+				})(items[i].matchId);
+
+
+			};
+
+
+		});
+
+  }, 
+  start: false,
+  timeZone: 'Europe/Berlin'
+});
+
+jobPullMatchData.start();
